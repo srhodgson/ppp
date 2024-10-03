be716fb01e87e26148904b555d6f579db85aab19
Stephen Hodgson <stephen@srhodgson.co.uk>
2024-10-03 22:35:15 +0100

    first commit

---
+++ build/Part01/Chapter01/CMakeFiles/hello_world.out.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/hello_world.out.dir/hello_world.cpp.o"
+  "CMakeFiles/hello_world.out.dir/hello_world.cpp.o.d"
+  "hello_world.out"
+  "hello_world.out.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/hello_world.out.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
