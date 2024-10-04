f3d82d8f0eb8d6d165f60c963cc936959e39d2ae
Stephen Hodgson <stephen@srhodgson.co.uk>
2024-10-04 21:44:04 +0100

    Update to handle unknown inputs

--- Part01/Chapter03/name_age.cpp
+++ Part01/Chapter03/name_age.cpp
@@ -4,10 +4,10 @@
 int main() 
 {
     cout << "Please enter your first name and age\n";
-    string first_name; // string variable 
-    int age;           // integer variable 
-    cin >> first_name; // read a string 
-    cin >> age;        // read an integer 
+    string first_name = "???"; // string variable
+                               // ("???" means "don't know the name") 
+    int age;                   // integer variable (0 means "don't know the age") 
+    cin >> first_name >> age;  // read a string followed by an integer 
     cout << "Hello, " << first_name << " (age " << age << ")\n";
     return 0;
 }
