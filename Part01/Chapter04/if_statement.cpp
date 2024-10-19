b53ded33fe4f9309d9a39ea9244450e81fb5f2c5
Stephen Hodgson <stephen@srhodgson.co.uk>
2024-10-19 21:43:05 +0100

    Start new chapter

---
+++ Part01/Chapter04/if_statement.cpp
@@ -0,0 +1,16 @@
+#include "../../std_lib_facilities.h"
+
+int main()
+{
+    int a = 0;
+    int b = 0;
+    cout << "Please enter two integers\n";
+    cin >> a >> b;
+
+    if (a < b) // condition
+        // 1st alternative (taken if condition is true)
+        cout << "max("<< a << "," << b << ") is " << b << "\n";
+    else 
+        // 2nd alternative (taken if condition is false)
+        cout << "max(" << a << "," << b << ") is " << a << "\n";
+}
